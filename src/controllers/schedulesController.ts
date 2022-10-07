87db34937cee2182bb8ba392796df3d2f0b1c6d6
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-07 15:12:02 -0300

    feat: set find schedule by userid controller

--- src/controllers/schedulesController.ts
+++ src/controllers/schedulesController.ts
@@ -4,9 +4,15 @@ import { TSchedule } from "../types/schedulesTypes";
 
 async function create(req: Request, res: Response) {
   const schedule: TSchedule = req.body;
-  const user_id = res.locals.user_id;
+  const user_id = +res.locals.user_id;
   const result = await schedulesService.create({ ...schedule, user_id });
   res.status(201).send(result);
 }
 
-export { create };
+async function findSchedulesByUserId(req: Request, res: Response) {
+  const user_id = +res.locals.user_id;
+  const result = await schedulesService.findSchedulesByUserId(user_id);
+  res.status(200).send(result);
+}
+
+export { create, findSchedulesByUserId };
