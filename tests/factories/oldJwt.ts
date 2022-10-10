1076065e87af4989f8ff51559ba13c5e0fa62cd6
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-10 13:41:19 -0300

    feat: factory old jwt token

---
+++ tests/factories/oldJwt.ts
@@ -0,0 +1,4 @@
+const oldJwt =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZpZ3VlaXJlZG8xNDk3QGdtYWlsLmNvbSIsImlhdCI6MTY2NTE5NTMzNCwiZXhwIjoxNjY1MjAyNTM0fQ.Gsscz9gG0kBatYh0h1BY-35o7N7-84tF5vC2OtKFoKc";
+
+export default oldJwt;
