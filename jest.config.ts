4a5c7acbaabb0ec2e0f1dff49a820120c8f37fcd
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-10-10 13:36:08 -0300

    sets up jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,27 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  collectCoverage: true,
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'repositories',
+    'jestGlobalMocks.ts',
+    '<rootDir>/src/server.ts',
+    '<rootDir>/src/database.ts',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/config',
+    '<rootDir>/tests/factories'
+  ],
 };
